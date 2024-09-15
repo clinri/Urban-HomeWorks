fd0bce64c8bd67aa84b57fd0b66538cfb2710911
Andrei Borisov <clinri@ya.ru>
2024-09-15 15:10:04 +0300

    homework java 14 - File

---
+++ java_14_File/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        AppendFileWriter appendFileWriter = new AppendFileWriter("java_14_File/src/textFile");
+        appendFileWriter.start();
+    }
+}
