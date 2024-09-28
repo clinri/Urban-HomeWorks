3ff5bfe45aad59b89ee9c01ae18d556a029d1e29
Andrei Borisov <clinri@ya.ru>
2024-09-28 23:03:21 +0300

    homework kotlin 01 - Variables

---
+++ kotlin_01_Variables/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val age: Int = 37
+    val name: String = "Andrei"
+    val isStudent: Boolean = false
+    val salary: Double = 120_000.11
+    println("$age|$name|$isStudent|$salary")
+}
